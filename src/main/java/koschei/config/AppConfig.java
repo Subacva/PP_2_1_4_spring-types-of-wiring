2b28edc5dd26a86c9bdd8c84311a94062b8b9ac4
Виталий <vitaljke02@gmail.com>
2023-09-22 02:06:48 +0300

    a

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,10 +1,11 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.spec.PSource;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
@@ -14,4 +15,7 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+
+
 }
